ece2c1e251fb8ee3c68c99ed4ad7b140231c7d37
yebaojia <695988558@qq.com>
2020-09-04 17:31:23 +0800

    详情页

---
+++ client.js
@@ -0,0 +1,45 @@
+const protobuf = require('protocol-buffers');
+const fs = require('fs');
+const EasySock = require('easy_sock');
+const schemas = protobuf(fs.readFileSync(`${__dirname}/detail-service/proto/detail.proto`));
+
+const easySock = new EasySock({
+  ip: '127.0.0.1',
+  port: 4000,
+  timeout: 500,
+  keepAlive: true
+})
+
+// 将需要发送的数据按协议进行二进制编码
+easySock.encode = function(data, seq) {
+  const body = schemas.ColumnRequest.encode(data);
+  const head = Buffer.alloc(8);
+  head.writeInt32BE(seq);
+  head.writeInt32BE(body.length, 4);
+  return Buffer.concat([head, body]);
+}
+
+// 将接收到的包进行解码，转换成js可操作的格式
+easySock.decode = function(buffer) {
+  const seq = buffer.readInt32BE();
+  const body = schemas.ColumnResponse.decode(buffer.slice(8));
+  return {
+    result: body,
+    seq
+  }
+}
+
+// 判断当前数据包是否完整
+easySock.isReceiveComplete = function(buffer) {
+  if (buffer.length < 8) {
+    return 0;
+  }
+  const bodyLength = buffer.readInt32BE(4);
+  if (bodyLength >= bodyLength + 8) {
+    return bodyLength + 8
+  } else {
+    return 0;
+  }
+}
+
+module.exports = easySock;
--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 const koa = require('koa');
 const mount = require('koa-mount');
 const static = require('koa-static');
-const fs = require('fs');
-const vm = require('vm');
+const rpcClient = require('./client');
+const template = require('./template/index.js');
 const app = new koa();
 
-const user = {
-  name: '<script />'
-}
-const templateMap = {
-  templateA: '`<h2>${include("templateB")}</h2>`',
-  templateB: '`<p>模板二</p>`'
-}
-const context = {
-  include: function(name) {
-    return templateMap[name]()
-  },
-  _: function(markup) {
-    if (!markup) return '';
-    return String(markup)
-      .replace(/&/g, '&amp;')
-      .replace(/ /g, '&nbsp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/\r{0,}\n/g, '<br/>')
-  }
-}
-Object.keys(templateMap).forEach(key => {
-  templateMap[key] = vm.runInNewContext(
-    `(function() {
-      return ${templateMap[key]}
-    })`
-    , context
-  )
-});
-console.log(templateMap['templateA']());
+const detailTemplate = template(__dirname + '/template/index.html');
 
-app.use(
-  static(__dirname + '/source/')
-)
+app.use(mount('/static', static(`${__dirname}/source/static/`)));
 
 app.use(
   mount('/', async (ctx) => {
-    ctx.body = fs.readFileSync(__dirname + '/source/index.html', 'utf-8');
+    if (!ctx.query.columnid) {
+      ctx.status = 400;
+      ctx.body = 'invalid columnid';
+      return false;
+    }
+
+    const result = await new Promise((resolve, reject) => {
+      rpcClient.write({
+        columnid: ctx.query.columnid
+      },
+      function(err, data) {
+        err ? reject(err) : resolve(data);
+      })
+    });
+    ctx.status = 200;
+    ctx.body = detailTemplate(result);
   })
 )
 
-app.listen(4000);
+app.listen(2000, () => {
+  console.log('server listened: http://localhost:2000');
+});
