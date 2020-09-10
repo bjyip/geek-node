b12e046656f7c334c4b12b1e4f22c3923a0fd572
yebaojia <695988558@qq.com>
2020-09-10 10:36:40 +0800

    路由

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ const detailTemplate = template(__dirname + '/template/index.html');
 
 app.use(mount('/static', static(`${__dirname}/source/static/`)));
 
-app.use(async (ctx) => {
-  if (!ctx.query.columnid) {
-    ctx.status = 400;
-    ctx.body = 'invalid columnid';
-    return false;
-  }
-
-  const result = await new Promise((resolve, reject) => {
-    rpcClient.write({
-      columnid: ctx.query.columnid
-    },
-    function(err, data) {
-      err ? reject(err) : resolve(data);
-    })
-  });
-  ctx.status = 200;
-  ctx.body = detailTemplate(result);
-})
+app.use(
+  mount('/detail', async (ctx) => {
+    if (!ctx.query.columnid) {
+      ctx.status = 400;
+      ctx.body = 'invalid columnid';
+      return false;
+    }
+    const result = await new Promise((resolve, reject) => {
+      rpcClient.write({
+        columnid: ctx.query.columnid
+      },
+      function(err, data) {
+        err ? reject(err) : resolve(data);
+      })
+    });
+    ctx.status = 200;
+    ctx.body = detailTemplate(result);
+  })
+)
 
 app.listen(3000, () => {
   console.log('server listened: http://localhost:3000');
