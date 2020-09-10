00bd8356d73f518e70069350691bb1ee4081c5e5
yebaojia <695988558@qq.com>
2020-09-10 14:30:03 +0800

    koa-graphql

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const mount = require('koa-mount');
 const static = require('koa-static');
 const rpcClient = require('./client');
 const template = require('./template/index.js');
+const graphqlHTTP = require('koa-graphql');
+
 const app = new koa();
 
 const detailTemplate = template(__dirname + '/template/index.html');
 
 app.use(mount('/static', static(`${__dirname}/source/static/`)));
 
+// 详情页
 app.use(
   mount('/detail', async (ctx) => {
     if (!ctx.query.columnid) {
@@ -28,6 +31,11 @@ app.use(
     ctx.body = detailTemplate(result);
   })
 )
+
+// koa-graphql例子
+app.use(mount('/graphql', graphqlHTTP({
+  schema: require('./schema.js')
+})));
 
 app.listen(3000, () => {
   console.log('server listened: http://localhost:3000');
